d896b0759add2ffba657046bfefdcb62f3e6513d
Maria Coitinho <brasildu@gmail.com>
2022-07-31 15:58:54 -0500

    commit tree 039c5171222a87fcf8e84f20126b858691ea26c4

--- index.js
+++ index.js
@@ -1,5 +1,96 @@
 /* Your Code Here */
 
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    const employee = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee;
+}
+console.log(createEmployeeRecord(["Maria", "Coitinho", "Engineer", 50]))
+
+
+
+
+function createEmployeeRecords(employee) {
+    const employeesObjArray = employee.map(arr => {
+        return createEmployeeRecord(arr)
+    })
+    return employeesObjArray;
+}
+
+
+
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+    // console.log("this: ", this)
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    })
+return this;
+}
+
+
+
+
+
+function createTimeOutEvent(dateStamp) {
+    let dateArray = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(dateArray[1], 10),
+        date: dateArray[0]
+    })
+return this;
+}
+
+
+
+
+
+function hoursWorkedOnDate(date) {
+    // console.log("this: ", this)
+    const objTimeOut = this.timeOutEvents.find(obj => obj.date === date);
+    const objTimeIn = this.timeInEvents.find(obj => obj.date === date);
+    return (objTimeOut.hour - objTimeIn.hour) / 100;
+}
+
+
+
+
+function wagesEarnedOnDate(date) {
+    // console.log('this: ', this)
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+}
+
+
+
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    console.log('this: ', this)
+    // const foundEmp = srcArray.find(obj => {
+    //     obj.firstName === firstName;
+    // })
+    // return foundEmp;
+}
+
+
+
+// function calculatePayroll(array) {
+
+
+// }
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
